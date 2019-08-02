9377261054dea4f27d144ef5fc01285c5468f1d2
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-08-02 19:31:29 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,17 @@
 # Code your solution here!
+
+def run_guessing_game
+  mind = rand(6) + 1
+  puts "Read my mind! What number am I thinking of?"
+  guess = gets.chomp
+  
+  if guess == mind.to_s
+    puts "You guessed the correct number!"
+  
+  elsif guess == "exit"
+    puts "Goodbye!"
+  
+  else
+    puts "Sorry! The computer guessed #{mind}."
+  end
+end
